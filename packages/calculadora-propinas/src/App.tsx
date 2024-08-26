97efa58c64515d04450678c8022387ef8ee9c68b
juanRCoder <guillecarck@gmail.com>
2024-08-26 18:34:32 -0500

    feat: commit

---
+++ packages/calculadora-propinas/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import BasicButtons from './components/BasicButtons'
+
+function App() {
+
+
+  return (
+    <>
+      <p className="tex-base underline text-red-400">Calculadora de propinas</p>
+      <BasicButtons />
+    </>
+  )
+}
+
+export default App
---
+++ packages/paleta-colores/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <p className="text-xl font-bold underline">Paleta de colores</p>
+    </>
+  )
+}
+
+export default App
