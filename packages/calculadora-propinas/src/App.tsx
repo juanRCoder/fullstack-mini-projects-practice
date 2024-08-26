6083c5173556d0ec2faa221472702a286bd9faf2
juanRCoder <guillecarck@gmail.com>
2024-08-26 18:53:50 -0500

    feat: segundo proyecto

---
+++ packages/calculadora-propinas/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <p className="text-3xl font-bold underline text-center cursor-pointer">Calculadora de propinas</p>
+    </>
+  )
+}
+
+export default App
