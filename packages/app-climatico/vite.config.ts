acf9f4970c5e4df317f94947b22d03b6e45a9782
juanRCoder <guillecarck@gmail.com>
2024-09-05 22:10:11 -0500

    Agregar flujo de trabajo de despliegue a GitHub Pages

--- packages/app-climatico/vite.config.ts
+++ packages/app-climatico/vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/proyectos-react/packages/app-climatico/',
   plugins: [react()],
 })
